b26fe98a240660344a62a4abf4a4310693526c7e
Tomasz Janowski <janaz9@gmail.com>
2012-07-10 21:52:16 +1000

    first commit

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source :rubygems
+
+# Project requirements
+gem 'rack'
+gem 'rake'
+gem 'nokogiri'
+gem 'sqlite3'
+
+# Component requirements
+gem 'rails'
+gem 'mysql2'
+gem 'sass'
+gem 'haml'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'jasmine'
+end
+
+
+# Test requirements
+group :test do
+  gem 'mongrel'
+  gem 'ruby-debug'
+  gem "capybara"
+
+  gem "selenium-webdriver"
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+  gem 'rcov'
+  gem "faker"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biletek::Application.config.secret_token = 'cb3c4d53c24b2b5a22e6b8463648c65057bee733837506983431c49cf31518371e6776bf1e7ebfa1047599bf0bace9d2c823d693f89dff67abeadcc02d2280a5'
